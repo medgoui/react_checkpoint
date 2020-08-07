30110a79328e17293900cb9707bdf0e2018a4a20
Mohamed Goui <goui.mohamed01@gmail.com>
2020-08-07 11:20:21 +0100

    adding new form with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import {Container,Row,Col,Button} from '../node_modules/react-bootstrap/';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Container>
+  <form class="text-center border border-light p-5" action="#!">
+<Row>
+<Col>
+<p class="h4 mb-4">Sign up</p>
+</Col>
+</Row>
+    <Row>
+        <Col><input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="First name" required /></Col>
+    </Row>
+    <hr/>
+    <Row>
+        <Col><input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Last name" required /></Col>
+    </Row>
+<hr/>
+<Row>
+<Col><input type="email" id="defaultRegisterFormEmail" class="form-control mb-4" placeholder="E-mail" required /></Col>
+</Row>
+<hr/>
+<Row>
+<Col><input type="password" id="defaultRegisterFormPassword" class="form-control" placeholder="Password" aria-describedby="defaultRegisterFormPasswordHelpBlock" required /></Col>
+</Row>
+<Row>
+  <Col><small id="defaultRegisterFormPasswordHelpBlock" class="form-text text-muted mb-4">
+    At least 8 characters and 1 digit
+</small></Col>
+</Row>
+<hr/>
+<Row>
+<Col><input type="text" id="defaultRegisterPhonePassword" class="form-control" placeholder="Phone number" aria-describedby="defaultRegisterFormPhoneHelpBlock" /></Col>
+</Row>
+<Col><small id="defaultRegisterFormPhoneHelpBlock" class="form-text text-muted mb-4">
+    Optional - for two step authentication
+</small>
+</Col>
+
+<div class="custom-control custom-checkbox">
+    <input type="checkbox" class="custom-control-input" id="defaultRegisterFormNewsletter" />
+    <label class="custom-control-label" for="defaultRegisterFormNewsletter">Subscribe to our newsletter</label>
+</div>
+
+<button id="signIN" class="btn btn-info my-4 btn-block" type="submit">Sign in</button>
+<div id="thankyou"></div>
+<Button as="input" variant="outline-danger" type="reset" value="RESET"/>
+<hr/>
+<p>or sign up with:</p>
+<a href="#" class="mx-2" role="button"><i class="fa fa-facebook"></i></a>
+<a href="#" class="mx-2" role="button"><i class="fa fa-twitter"></i></a>
+<a href="#" class="mx-2" role="button"><i class="fa fa-linkedin"></i></a>
+<a href="#" class="mx-2" role="button"><i class="fa fa-github"></i></a>
+
+<hr/>
+
+
+<p>By clicking
+    <em> Sign up </em> you agree to our
+    <a href="" target="_blank"> terms of service</a>
+</p>
+</form>
+</Container>
+  </>
   );
 }
 
